57402fe3d77be9fc5b324fb04956a797c57109e1
Edik123123 <edikcrimedog@gmail.com>
2020-08-20 17:21:16 +0300

    firs commit of OHB

---
+++ script.js
@@ -0,0 +1,21 @@
+let slot1 = document.getElementById('slot1');
+let imgArray = [];
+
+imgArray[0] = new Image();
+imgArray[0].src = './img/diamond.png';
+
+imgArray[1] = new Image();
+imgArray[1].src = './img/horseshoe.png';
+
+imgArray[2] = new Image();
+imgArray[2].src = './img/seven.png';
+
+imgArray[3] = new Image();
+imgArray[3].src = './img/bell.png';
+
+imgArray[4] = new Image();
+imgArray[4].src = './img/cherry.png';
+
+imgArray[5] = new Image();
+imgArray[5].src = './img/lemon.png';
+
